6c07f3c00fb525c8a86a67a8fae728238834e3a4
RichardoTS <ri.tapiasaez@gmail.com>
2023-05-19 07:42:56 +0000

    Minor changes

--- src/models.py
+++ src/models.py
@@ -3,8 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
@@ -14,6 +16,122 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
+            "username": self.username,
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+class Character(db.Model):
+    __tablename__ = "characters"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    mass = db.Column(db.Integer, nullable=False)
+    height = db.Column(db.Integer, nullable=False)
+    gender = db.Column(db.String(8), unique=False, nullable=False)
+    birth_year = db.Column(db.String(10), unique=False, nullable=False)
+    eye_color = db.Column(db.String(20), unique=False, nullable=False)
+    skin_color = db.Column(db.String(20), unique=False, nullable=False)
+    hair_color = db.Column(db.String(20), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<Character %r>' % self.username
+
+    def serialize(self):
+        return{
+            "id": self.id,
+            "name": self.name,
+            "mass": self.mass,
+            "height": self.height,
+            "gender": self.gender,
+            "birth_year": self.birth_year,
+            "eye_color": self.eye_color,
+            "skin_color": self.skin_color,
+            "hair_color": self.hair_color
+        }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+
+class Planet(db.Model):
+    __tablename__ = "planets"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique=True, nullable=False)
+    population = db.Column(db.Integer, unique=False, nullable=False)
+    gravity = db.Column(db.String(50), unique=False, nullable=False)
+    diameter = db.Column(db.Integer, unique=False, nullable=False)
+    climate = db.Column(db.String(20), unique=False, nullable=False)
+    terrain = db.Column(db.String(20), unique=False, nullable=False)
+    surface_water = db.Column(db.Integer, unique=False, nullable=False)
+    rotation_period = db.Column(db.Integer, unique=False, nullable=False)
+    orbital_period = db.Column(db.Integer, unique=False, nullable=False)
+
+    def serialize(self):
+        return{
+            "id": self.id,
+            "name": self.name,
+            "population": self.population,
+            "gravity": self.gravity,
+            "diameter": self.diameter,
+            "climate": self.climate,
+            "terrain": self.terrain,
+            "surface_water": self.surface_water,
+            "rotation_period": self.rotation_period,
+            "orbital_period": self.orbital_period
+        }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+			
+class Favourite(db.Model):
+    __tablename__ = "favourites"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    character_id = db.Column(db.Integer, db.ForeignKey("characters.id"))
+    planet_id = db.Column(db.Integer, db.ForeignKey("planets.id"))
+
+    def serialize(self):
+        return{
+            "id": self.id,
+            "user_id": self.user_id,
+            "character_id": self.character_id,
+            "planet_id": self.planet_id        
+        }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
